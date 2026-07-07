c46fe8f3816111521ac23db4ccaa886d022dc325
maxmaxmori-rgb <maxmaxmori@gmail.com>
2026-07-07 22:39:07 +0200

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,11 @@
 // Incolla qui la configurazione Firebase.
 // IMPORTANTE: deve essere window.firebaseConfig = { ... }
 window.firebaseConfig = {
-  apiKey: "INSERISCI_API_KEY",
-  authDomain: "INSERISCI_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://INSERISCI_PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "INSERISCI_PROJECT_ID",
-  storageBucket: "INSERISCI_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "INSERISCI_SENDER_ID",
-  appId: "INSERISCI_APP_ID"
+  apiKey: "AIzaSyCkuzUnIG5719y59GRStCYsdOvd1GtnOZo",
+  authDomain: "dart-master-call.firebaseapp.com",
+  databaseURL: "https://dart-master-call-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dart-master-call",
+  storageBucket: "dart-master-call.firebasestorage.app",
+  messagingSenderId: "604467965067",
+  appId: "1:604467965067:web:cced31c47f095f0b4db88e"
 };
